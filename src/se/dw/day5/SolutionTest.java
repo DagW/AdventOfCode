c50da67d9ce4bf95bd88d89463017c6c6c10e580
Dag Wullt <dag.wullt@elasticmobile.se>
2017-06-04 20:27:51 +0200

    Day5 solution

---
+++ src/se/dw/day5/SolutionTest.java
@@ -0,0 +1,29 @@
+package se.dw.day5;
+
+import org.junit.Test;
+
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+
+    public final static String exampleinput = "abc";
+    public final static String input = "ffykfhsq";
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testSolution() throws NoSuchAlgorithmException {
+        assertEquals(new Solution(exampleinput).password, "18F47A30");
+        assertEquals(new Solution(input).password, "C6697B55");
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+}
