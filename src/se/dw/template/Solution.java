acb020112f4ae43b75035f2c492d8817c4aa0c9e
Dag Wullt <dag.wullt@elasticmobile.se>
2017-06-04 11:28:06 +0200

    Add template

---
+++ src/se/dw/template/Solution.java
@@ -0,0 +1,18 @@
+package se.dw.template;
+
+/**
+ * My solution to the day2 problem
+ * http://adventofcode.com/2016/day/2
+ */
+public class Solution {
+
+    public final static String input = "";
+
+
+    public Solution(String input) { }
+
+    public static void main(String[] args) {
+        new Solution(input);
+    }
+
+}
