4b60fd22aca9c343828c9bbf1cf89240619c284b
Dag Wullt <wulltd@gmail.com>
2018-12-01 19:57:32 +0100

    Day1 2018

---
+++ aoc_python/2018/day1/solution.py
@@ -0,0 +1,36 @@
+def part1(inputArray):
+    sum = 0
+    for item in inputArray:
+        sum += int(item)
+    return sum
+
+
+def part2(inputArray):
+    sum = 0
+    seenFrequencies = {0: True}
+    while True:
+        for item in inputArray:
+            sum += int(item)
+            if sum in seenFrequencies:
+                return sum
+            seenFrequencies[sum] = True
+
+
+def readFile(filename):
+    return open(filename).read().strip().split("\n")
+
+
+if __name__ == "__main__":
+    assert part1([+1, -2, +3, +1]) == 3
+    assert part1([+1, +1, +1]) == 3
+    assert part1([+1, +1, -2]) == 0
+    assert part1([-1, -2, -3]) == -6
+
+    assert part2([+1, -1]) == 0
+    assert part2([+3, +3, +4, -2, -4]) == 10
+    assert part2([-6, +3, +8, +5, -6]) == 5
+    assert part2([+7, +7, -2, -7, -4]) == 14
+
+    inputData = readFile("input")
+    print(part1(inputData))
+    print(part2(inputData))
