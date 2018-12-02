3c1495188e17c63119a5c7198544a7103f38f99e
Dag Wullt <wulltd@gmail.com>
2018-12-02 12:45:45 +0100

    Day2Part2

--- aoc_python/2018/day2/solution.py
+++ aoc_python/2018/day2/solution.py
@@ -11,12 +11,40 @@ def part1(inputArray):
             threeTimes += 1
     return twoTimes * threeTimes
 
+
+def part2(inputArray):
+    # Create table of how the rows compare to each of the other rows
+    # How many chars are matching?
+    stringmap = {}
+    for string in inputArray:
+        stringmap[string] = {}
+        for string2 in [i for i in inputArray if i != string]:
+            stringmap[string][string2] = 0
+            for charIndex in range(0, len(string)):
+                if string[charIndex] == string2[charIndex]:
+                    stringmap[string][string2] += 1
+
+    # If they match all but one char,
+    # return all matching chars
+    stringLenght = len(inputArray[0]) - 1
+    match = ""
+    for left, matchList in stringmap.items():
+        for right, value in matchList.items():
+            if value == stringLenght:
+                for charIndex in range(0, len(left)):
+                    if left[charIndex] == right[charIndex]:
+                        match += str(left[charIndex])
+                return match
+
+
 def readFile(filename):
     return open(filename).read().strip().split("\n")
 
 
 if __name__ == "__main__":
-    assert part1(["abcdef","bababc","abbcde","abcccd","aabcdd","abcdee","ababab"]) == 12
+    assert part1(["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"]) == 12
+    assert part2(["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]) == "fgij"
 
     inputData = readFile("input")
-    print(part1(inputData))
+    assert part1(inputData) == 6150
+    assert part2(inputData) == "rteotyxzbodglnpkudawhijsc"
