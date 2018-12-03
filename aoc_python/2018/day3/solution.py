70f294338c1a28f9f7b10194ee030e6e81d8792d
Dag Wullt <wulltd@gmail.com>
2018-12-03 11:16:03 +0100

    numpy matrix selections and part2

--- aoc_python/2018/day3/solution.py
+++ aoc_python/2018/day3/solution.py
@@ -11,22 +11,30 @@ def part1(inputArray):
         c_y = int(matches.group(3))
         c_w = int(matches.group(4))
         c_h = int(matches.group(5))
+        cloth[c_x : c_x + c_w, c_y : c_y + c_h] += 1
+    return len(np.where(cloth > 1)[0])
 
-        for x in range(0, len(cloth)):
-            for y in range(0, len(cloth[0])):
-                cloth_position = cloth[x][y]
-                if x >= c_x and x < c_x + c_w and y >= c_y and y < c_y + c_h:
-                    if cloth_position == 0:
-                        cloth[x][y] = c_id
-                    else:
-                        cloth[x][y] = -1
-    sum = 0
-    for x in range(0, len(cloth)):
-        for y in range(0, len(cloth[0])):
-            if cloth[x][y] == -1:
-                sum += 1
-    print("cloth collisions:", sum)
-    return sum
+
+def part2(inputArray):
+    cloth = np.zeros((1000, 1000))
+    for claim in inputArray:
+        matches = re.compile("^#([\d]+) @ ([\d]+),([\d]+): ([\d]+)x([\d]+)$").match(claim)
+        c_id = int(matches.group(1))
+        c_x = int(matches.group(2))
+        c_y = int(matches.group(3))
+        c_w = int(matches.group(4))
+        c_h = int(matches.group(5))
+        cloth[c_x : c_x + c_w, c_y : c_y + c_h] += 1
+
+    for claim in inputArray:
+        matches = re.compile("^#([\d]+) @ ([\d]+),([\d]+): ([\d]+)x([\d]+)$").match(claim)
+        c_id = int(matches.group(1))
+        c_x = int(matches.group(2))
+        c_y = int(matches.group(3))
+        c_w = int(matches.group(4))
+        c_h = int(matches.group(5))
+        if sum(sum(cloth[c_x : c_x + c_w, c_y : c_y + c_h])) == c_w * c_h:
+            return c_id
 
 
 def readFile(filename):
@@ -34,7 +42,9 @@ def readFile(filename):
 
 
 if __name__ == "__main__":
-    assert part1(["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2", "#2 @ 253,106: 10x25"]) == 4
+    assert part1(["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"]) == 4
+    assert part2(["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"]) == 3
 
     inputData = readFile("input")
-    assert part1(inputData)
+    print(part1(inputData))
+    print(part2(inputData))
