edb6f413c9f1dffd2888e2e041844bf941d3eacb
Dag Wullt <wulltd@gmail.com>
2018-12-19 21:08:29 +0100

    part2

--- aoc_python/2018/day8/solution.py
+++ aoc_python/2018/day8/solution.py
@@ -1,5 +1,10 @@
-def readFile(filename):
-    return open(filename).read().strip().split("\n")
+def part1(inputArray):
+    items = list(map(int, inputArray[0].split(" ")))
+
+    _, children = parseNodes(items)
+    sum = sumChildrenKeys(children)
+
+    return sum
 
 
 def parseNodes(items, depth=0):
@@ -11,35 +16,45 @@ def parseNodes(items, depth=0):
         items, child = parseNodes(items, depth + 1)
         children.append(child)
 
-    meta = items[:nbrmeta]
+    meta = ",".join(str(x) for x in items[:nbrmeta])
 
-    # print(nbrchildren, nbrmeta, meta)
+    # return the input array without myself and child nodes
     items = items[nbrmeta:]
-
-    return items, {sum(meta): children}
+    return items, {meta: children}
 
 
 def sumChildrenKeys(children):
-    sum = 0
+    retSum = 0
     for key, value in children.items():
-        sum += key
+        retSum += sum([int(c) for c in key.split(",")])
         for item in value:
-            sum += sumChildrenKeys(item)
-    return sum
-
-
-def part1(inputArray):
-    items = list(map(int, inputArray[0].split(" ")))
-
-    _, children = parseNodes(items)
-    sum = sumChildrenKeys(children)
-
-    return sum
+            retSum += sumChildrenKeys(item)
+    return retSum
 
 
 def part2(inputArray):
-    # WIP
-    return 66
+    items = list(map(int, inputArray[0].split(" ")))
+
+    _, children = parseNodes(items)
+    return sumPart2(children)
+
+
+def sumPart2(nodes):
+    retSum = 0
+
+    for meta, childNodes in nodes.items():
+        if len(childNodes) == 0:
+            retSum += sum([int(c) for c in meta.split(",")])
+        for index in meta.split(","):
+            try:
+                retSum += sumPart2(childNodes[int(index) - 1])
+            except:
+                pass
+    return retSum
+
+
+def readFile(filename):
+    return open(filename).read().strip().split("\n")
 
 
 if __name__ == "__main__":
@@ -47,5 +62,5 @@ if __name__ == "__main__":
     assert part2(["2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"]) == 66
 
     inputData = readFile("input")
-    # print(part1(inputData))
-    # print(part2(inputData))
+    print(part1(inputData))
+    print(part2(inputData))
