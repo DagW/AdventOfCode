844f48e53b3a68cd14aa19bcfed2ece920c4a711
Dag Wullt <wulltd@gmail.com>
2020-10-11 20:01:09 +0200

    Fixed the recursion, part2 solution and added a queue alternative for comparison

--- aoc_go/2019/day14/main.go
+++ aoc_go/2019/day14/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 type Ingredient struct {
 	Amount int
@@ -17,148 +25,214 @@ type Reaction struct {
 	To   Ingredient
 	From []Ingredient
 }
-type BaseCost struct {
-	To   Ingredient
-	From Ingredient
+type Reactions struct {
+	reactions []Reaction
 }
 
-func RecurseRequirements(
-	requirements map[string]int,
-	reactions []Reaction,
-	ingredientName string,
-	multiplier int,
-) (map[string]int, bool) {
-	reaction, found := FindReaction(reactions, ingredientName)
-	if !found {
-		return requirements, false
-	}
-	rest := multiplier % reaction.To.Amount
-	multiplier = multiplier / reaction.To.Amount
-	if rest != 0 {
-		multiplier += 1
-	}
-
-	for _, reactionIngredient := range reaction.From {
-		newReq, success := RecurseRequirements(
-			requirements, reactions,
-			reactionIngredient.Name,
-			reactionIngredient.Amount*multiplier,
-		)
-		if success {
-			requirements = newReq
-		} else {
-			//If this produces more than we need? How do we tell?
-			fmt.Println("Added",reactionIngredient.Amount * multiplier,reactionIngredient.Name)
-			requirements[reactionIngredient.Name] += reactionIngredient.Amount * multiplier
-		}
-	}
-	return requirements, true
-}
-
-func SolvePart1(str string) int {
-	baseCosts, reactions := parseInput(str)
-	fmt.Println("baseCosts", baseCosts)
-	fmt.Println("reactions", reactions)
-	// Collect the full requirements (All costs, ex 27xA, 2xB etc)
-	requirements := map[string]int{}
-	requirements, _ = RecurseRequirements(
-		requirements, reactions,
-		"FUEL", 1,
-	)
-	// Calculate the cost in ore
-	//excess_resources := map[string]int{}
-	fmt.Println(requirements)
-	ore := 0
-	for k, v := range requirements {
-		baseCost, _ := FindBaseCost(baseCosts, k)
-		times := v / baseCost.To.Amount
-		rest := v % baseCost.To.Amount
-		if rest > 0 {
-			times++
-		}
-		ore += baseCost.From.Amount * times
-		fmt.Println("Cost of", v, k, "->", baseCost.From.Amount*times, "ORE")
-	}
-	return ore
-}
-
-func FindReaction(reactions []Reaction, target string) (Reaction, bool) {
-	for _, reaction := range reactions {
+func (reactions *Reactions) FindReaction(target string) (Reaction, bool) {
+	for _, reaction := range reactions.reactions {
 		if reaction.To.Name == target {
 			return reaction, true
 		}
 	}
 	return Reaction{}, false
 }
-func FindBaseCost(baseCosts []BaseCost, target string) (BaseCost, bool) {
-	for _, baseCost := range baseCosts {
-		if baseCost.To.Name == target {
-			return baseCost, true
-		}
-	}
-	return BaseCost{}, false
+func (reactions *Reactions) add(reaction Reaction) {
+	reactions.reactions = append(reactions.reactions, reaction)
 }
 
-func parseInput(str string) ([]BaseCost, []Reaction) {
-	var baseCosts []BaseCost
-	var reactions []Reaction
-	for _, row := range strings.Split(str, "\n") {
-		if strings.TrimSpace(row) != "" {
-			sides := strings.Split(row, " => ")
-			if strings.Contains(sides[0], "ORE") {
-				toAmount, _ := strconv.Atoi(strings.Split(sides[1], " ")[0])
-				toName := strings.Split(sides[1], " ")[1]
-				fromAmount, _ := strconv.Atoi(strings.Split(sides[0], " ")[0])
-				fromName := strings.Split(sides[0], " ")[1]
-				baseCosts = append(baseCosts, BaseCost{
-					From: Ingredient{
-						Amount: fromAmount,
-						Name:   fromName,
-					},
-					To: Ingredient{
-						Amount: toAmount,
-						Name:   toName,
-					},
-				})
-			} else {
-				var from []Ingredient
-				for _, val := range strings.Split(sides[0], ", ") {
-					fromName := strings.Split(val, " ")[1]
-					fromAmount, _ := strconv.Atoi(strings.Split(val, " ")[0])
-					from = append(from, Ingredient{
-						Amount: fromAmount,
-						Name:   fromName,
-					})
+type Stack struct {
+	items []Ingredient
+}
+
+func (stack *Stack) push(ingredient Ingredient) {
+	stack.items = append(stack.items, ingredient)
+}
+func (stack *Stack) pop() Ingredient {
+	ingredient := stack.items[len(stack.items)-1]
+	stack.items = stack.items[:len(stack.items)-1]
+	return ingredient
+}
+func (stack *Stack) empty() bool {
+	return len(stack.items) == 0
+}
+
+func (stack *Stack) Clear() {
+	stack.items = []Ingredient{}
+}
+
+const TRILLION int = 1_000_000_000_000
+
+func SolvePart2(str string, solverFunction func(reactions Reactions, numFuel int) int) int {
+	reactions := parseInput(str)
+	fuel := 1.0
+	runs := 0
+	stepsize := 1000_000
+	for stepsize > 0 {
+		runs++
+		ore := solverFunction(reactions, int(fuel))
+		if float64(ore)/float64(TRILLION) > 1 {
+			fuel -= float64(stepsize)
+			stepsize /= 2
+			fuel += float64(stepsize)
+		} else {
+			fuel += float64(stepsize)
+		}
+	}
+
+	return int(fuel)
+}
+
+func SolvePart1(str string, solverFunction func(reactions Reactions, numFuel int) int) int {
+	reactions := parseInput(str)
+	return solverFunction(reactions, 1)
+}
+
+func findOreForReactionsWithRecursion(reactions Reactions, numFuel int) int {
+	supplies := map[string]int{}
+	return recurse(Ingredient{Name: "FUEL", Amount: numFuel}, &reactions, &supplies)
+}
+
+func recurse(ingredient Ingredient, reactions *Reactions, supplies *map[string]int) int {
+	if ingredient.Name == "ORE" {
+		return ingredient.Amount
+	}
+	if amountInSupply := int(math.Min(float64((*supplies)[ingredient.Name]), float64(ingredient.Amount))); amountInSupply > 0 {
+		ingredient.Amount -= amountInSupply
+		(*supplies)[ingredient.Name] -= amountInSupply
+	}
+	if ingredient.Amount == 0 {
+		return 0
+	}
+	ore := 0
+	if reaction, found := reactions.FindReaction(ingredient.Name); found {
+		multiplier := int(math.Ceil(float64(ingredient.Amount) / float64(reaction.To.Amount)))
+		for _, subingredient := range reaction.From {
+			subingredient.Amount *= multiplier
+			ore += recurse(subingredient, reactions, supplies)
+		}
+		if extraSupplies := (multiplier * reaction.To.Amount) - ingredient.Amount; extraSupplies > 0 {
+			(*supplies)[reaction.To.Name] += extraSupplies
+		}
+	}
+	return ore
+}
+
+func findOreForReactionsWithQueue(reactions Reactions, numFuel int) int {
+	supplies := map[string]int{}
+	var stack Stack
+	stack.push(Ingredient{
+		Name:   "FUEL",
+		Amount: numFuel,
+	})
+	ore := 0
+	for !stack.empty() {
+		ingredient := stack.pop()
+		if ingredient.Name == "ORE" {
+			ore += ingredient.Amount
+		} else {
+			//Check if we have some first
+			if amountInSupply := int(math.Min(float64(supplies[ingredient.Name]), float64(ingredient.Amount))); amountInSupply > 0 {
+				ingredient.Amount -= amountInSupply
+				supplies[ingredient.Name] -= amountInSupply
+			}
+			// fmt.Println("Searching for cost of", ingredient.Amount, ingredient.Name)
+			if reaction, found := reactions.FindReaction(ingredient.Name); found {
+				// fmt.Println("  found that it needs ", reaction.From, "to produce", reaction.To.Amount, reaction.To.Name)
+				multiplier := int(math.Ceil(float64(ingredient.Amount) / float64(reaction.To.Amount)))
+				// fmt.Println("  So we need to run the reaction", multiplier, "times to produce", reaction.To.Amount*multiplier, reaction.To.Name)
+				for _, subingredient := range reaction.From {
+					subingredient.Amount = subingredient.Amount * multiplier
+					stack.push(subingredient)
 				}
-				toAmount, _ := strconv.Atoi(strings.Split(sides[1], " ")[0])
-				toName := strings.Split(sides[1], " ")[1]
-				reactions = append(reactions, Reaction{
-					From: from,
-					To: Ingredient{
-						Amount: toAmount,
-						Name:   toName,
-					},
-				})
+				if extraSupplies := (multiplier * reaction.To.Amount) - ingredient.Amount; extraSupplies > 0 {
+					supplies[reaction.To.Name] += extraSupplies
+					// fmt.Println("We received some extra from this reaction", supplies)
+				}
 			}
 		}
 	}
-	return baseCosts, reactions
+
+	return ore
 }
-func main() {
-	file, err := os.Open("aoc_go/2019/day14/test4")
+
+func parseInput(str string) Reactions {
+	var reactions Reactions
+	for _, row := range strings.Split(str, "\n") {
+		if strings.TrimSpace(row) != "" {
+			sides := strings.Split(row, " => ")
+			var from []Ingredient
+			for _, val := range strings.Split(sides[0], ", ") {
+				fromName := strings.Split(val, " ")[1]
+				fromAmount, _ := strconv.Atoi(strings.Split(val, " ")[0])
+				from = append(from, Ingredient{
+					Amount: fromAmount,
+					Name:   fromName,
+				})
+			}
+			toAmount, _ := strconv.Atoi(strings.Split(sides[1], " ")[0])
+			toName := strings.Split(sides[1], " ")[1]
+			reactions.add(Reaction{
+				From: from,
+				To: Ingredient{
+					Amount: toAmount,
+					Name:   toName,
+				},
+			})
+		}
+	}
+	return reactions
+}
+
+func readInput(path string) string {
+	file, err := os.Open(path)
 	check(err)
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	input := ""
+	contents := ""
 	for scanner.Scan() {
-		input += scanner.Text() + "\n"
+		contents += scanner.Text() + "\n"
 	}
-	fmt.Println("Part1 solution:", SolvePart1(input))
+	return contents
 }
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+func main() {
+	/*for _, filename := range []string{""}{
+		fmt.Println("Part1", filename, SolvePart1(readInput("aoc_go/2019/day14/test0")))
+	}*/
+	fmt.Println("findOreForReactionsWithRecursion")
+	fmt.Println("Part1 test1:", SolvePart1(readInput("aoc_go/2019/day14/test1"), findOreForReactionsWithRecursion), "31")
+	fmt.Println("Part1 test2:", SolvePart1(readInput("aoc_go/2019/day14/test2"), findOreForReactionsWithRecursion), "165")
+	fmt.Println("Part1 test3:", SolvePart1(readInput("aoc_go/2019/day14/test3"), findOreForReactionsWithRecursion), "13312")
+	fmt.Println("Part1 test4:", SolvePart1(readInput("aoc_go/2019/day14/test4"), findOreForReactionsWithRecursion), "180697")
+	fmt.Println("Part1 test5:", SolvePart1(readInput("aoc_go/2019/day14/test5"), findOreForReactionsWithRecursion), "2210736")
+	fmt.Println("Part1 solution:", SolvePart1(readInput("aoc_go/2019/day14/input"), findOreForReactionsWithRecursion), "114125")
+	fmt.Println("Part2 test3:", SolvePart2(readInput("aoc_go/2019/day14/test3"), findOreForReactionsWithRecursion), "82892753")
+	fmt.Println("Part2 test4:", SolvePart2(readInput("aoc_go/2019/day14/test4"), findOreForReactionsWithRecursion), "5586022")
+	fmt.Println("Part2 test5:", SolvePart2(readInput("aoc_go/2019/day14/test5"), findOreForReactionsWithRecursion), "460664")
+	fmt.Println("Part2 solution:", SolvePart2(readInput("aoc_go/2019/day14/input"), findOreForReactionsWithRecursion), "12039407")
+
+	fmt.Println("findOreForReactionsWithQueue")
+	fmt.Println("Part1 test1:", SolvePart1(readInput("aoc_go/2019/day14/test1"), findOreForReactionsWithQueue), "31")
+	fmt.Println("Part1 test2:", SolvePart1(readInput("aoc_go/2019/day14/test2"), findOreForReactionsWithQueue), "165")
+	fmt.Println("Part1 test3:", SolvePart1(readInput("aoc_go/2019/day14/test3"), findOreForReactionsWithQueue), "13312")
+	fmt.Println("Part1 test4:", SolvePart1(readInput("aoc_go/2019/day14/test4"), findOreForReactionsWithQueue), "180697")
+	fmt.Println("Part1 test5:", SolvePart1(readInput("aoc_go/2019/day14/test5"), findOreForReactionsWithQueue), "2210736")
+	fmt.Println("Part1 solution:", SolvePart1(readInput("aoc_go/2019/day14/input"), findOreForReactionsWithQueue), "114125")
+	fmt.Println("Part2 test3:", SolvePart2(readInput("aoc_go/2019/day14/test3"), findOreForReactionsWithQueue), "82892753")
+	fmt.Println("Part2 test4:", SolvePart2(readInput("aoc_go/2019/day14/test4"), findOreForReactionsWithQueue), "5586022")
+	fmt.Println("Part2 test5:", SolvePart2(readInput("aoc_go/2019/day14/test5"), findOreForReactionsWithQueue), "460664")
+	fmt.Println("Part2 solution:", SolvePart2(readInput("aoc_go/2019/day14/input"), findOreForReactionsWithQueue), "12039407")
+
+	start := time.Now()
+	for i := 0; i < 10_000; i++ {
+		SolvePart1(readInput("aoc_go/2019/day14/input"), findOreForReactionsWithRecursion)
 	}
+	fmt.Println("With recursion", time.Since(start)) // 2.677544662s
+	start = time.Now()
+	for i := 0; i < 10_000; i++ {
+		SolvePart1(readInput("aoc_go/2019/day14/input"), findOreForReactionsWithQueue)
+	}
+	fmt.Println("With queue", time.Since(start)) // 3.650587617s
 }
