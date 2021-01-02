e97a2cf7d2af5cdd66f06220d20118c181f02719
Dag Wullt <wulltd@gmail.com>
2021-01-02 17:19:52 +0100

    11

--- aoc_go/2020/day11/main.go
+++ aoc_go/2020/day11/main.go
@@ -7,20 +7,20 @@ import (
 )
 
 const (
-	FREE     = iota
-	OCCUPIED = iota
-	FLOOR    = iota
+	empty = iota
+	occupied
+	floor
 )
 
 func printMap(m [][]int) {
 	fmt.Println("----")
 	for _, r := range m {
 		for _, c := range r {
-			if c == FREE {
+			if c == empty {
 				fmt.Print("L")
-			} else if c == OCCUPIED {
+			} else if c == occupied {
 				fmt.Print("#")
-			} else if c == FLOOR {
+			} else if c == floor {
 				fmt.Print(".")
 			}
 		}
@@ -29,85 +29,96 @@ func printMap(m [][]int) {
 	fmt.Println("----")
 }
 
-func allAroundMeAreFree(m [][]int, y, x int) bool {
-	test := [4][2]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
+func noOccupied(m [][]int, y, x, r int) bool {
+	test := [8][2]int{
+		{-1, 1},
+		{0, 1},
+		{1, 1},
+		{-1, 0},
+		{1, 0},
+		{-1, -1},
+		{0, -1},
+		{1, -1},
+	}
 	for _, t := range test {
-		tx, ty := x-t[1], y-t[0]
-		if ty > 0 || tx > 0 || ty < len(m) || tx < len(m[1]) {
+		//for multiplier := 1; multiplier < len(m); multiplier++ {
+		tx, ty := x+t[1], y+t[0]
+		//tx, ty = tx*multiplier, ty*multiplier
+		if ty < 0 || tx < 0 ||
+			ty > len(m)-1 || tx > len(m[1])-1 {
 			continue
 		}
-		fmt.Println(tx, ty)
-		if m[ty][tx] == OCCUPIED {
+		if m[ty][tx] == occupied {
 			return false
 		}
+		//}
 	}
 	return true
 }
-func allAroundMeAreOccupied(m [][]int, y, x int) bool {
-	test := [4][2]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
+func numOccupied(m [][]int, y, x, limit int) bool {
+	test := [8][2]int{
+		{-1, 1},
+		{0, 1},
+		{1, 1},
+		{-1, 0},
+		{1, 0},
+		{-1, -1},
+		{0, -1},
+		{1, -1},
+	}
+	numOccupied := 0
 	for _, t := range test {
-		tx, ty := x-t[1], y-t[0]
-		if ty > 0 || tx > 0 || ty < len(m) || tx < len(m[1]) {
+		tx, ty := x+t[1], y+t[0]
+		if ty < 0 || tx < 0 || ty > len(m)-1 || tx > len(m[1])-1 {
 			continue
 		}
-		fmt.Println(tx, ty)
-		if m[ty][tx] == FREE {
-			return false
+		if m[ty][tx] == occupied {
+			numOccupied++
+		}
+		if numOccupied >= limit {
+			return true
 		}
 	}
-	return true
+	return false
 }
 
-func part1(m [][]int) int {
-	fmt.Println("INPUT")
-	printMap(m)
-	fmt.Println("STARTING")
-	changes := -1
-	count := 0
-	for changes != 0 && count != 10 {
-
-		fmt.Println()
-		fmt.Println(m)
-		fmt.Println()
-		fmt.Println("BEFORE", changes)
-		printMap(m)
-		count++
-		changes = 0
-		m2 := make([][]int, len(m))
-		for i := range m {
-			m2[i] = make([]int, len(m[i]))
-			copy(m2[i], m[i])
-		}
-		for y, r := range m {
-			for x, c := range r {
-				if c == FREE && allAroundMeAreFree(m, y, x) {
-					m2[y][x] = OCCUPIED
-					changes++
-				}
-			}
-		}
-		for y, r := range m2 {
-			for x, c := range r {
-				if c == OCCUPIED && allAroundMeAreOccupied(m, y, x) {
-					m2[y][x] = FREE
-					changes++
-				}
-			}
-		}
-
-		fmt.Println(count, "AFTER", changes)
-		m = m2
-		printMap(m)
+func doRound(m [][]int, r, limit int) ([][]int, int) {
+	changes := 0
+	newMap := make([][]int, len(m))
+	for y := range m {
+		newMap[y] = make([]int, len(m[0]))
 	}
-	sum := 0
-	for _, r := range m {
-		for _, c := range r {
-			if c == OCCUPIED {
-				sum++
+	for y, row := range m {
+		for x, col := range row {
+			if col == empty && noOccupied(m, y, x, r) {
+				newMap[y][x] = occupied
+				changes++
+			} else if col == occupied && numOccupied(m, y, x, limit) {
+				newMap[y][x] = empty
+				changes++
+			} else {
+				newMap[y][x] = col
+			}
+
+		}
+	}
+	return newMap, changes
+}
+
+func doRounds(m [][]int, r, limit int) int {
+	changes := 1
+	for changes > 0 {
+		m, changes = doRound(m, r, limit)
+	}
+	num := 0
+	for _, row := range m {
+		for _, col := range row {
+			if col == occupied {
+				num++
 			}
 		}
 	}
-	return sum
+	return num
 }
 
 func readFile(filename string) (seats [][]int) {
@@ -120,11 +131,11 @@ func readFile(filename string) (seats [][]int) {
 		row := make([]int, 0)
 		for _, c := range line {
 			if c == '.' {
-				row = append(row, FLOOR)
+				row = append(row, floor)
 			} else if c == '#' {
-				row = append(row, OCCUPIED)
+				row = append(row, occupied)
 			} else if c == 'L' {
-				row = append(row, FREE)
+				row = append(row, empty)
 			}
 		}
 		seats = append(seats, row)
@@ -133,6 +144,10 @@ func readFile(filename string) (seats [][]int) {
 }
 
 func main() {
-	fmt.Println(part1(readFile("aoc_go/2020/day11/test")))
+	test, input := readFile("test"), readFile("input")
+	fmt.Println(doRounds(test, 1, 4), 37)
+	fmt.Println(doRounds(input, 1, 4), 2261)
 	//fmt.Println(readFile("aoc_go/2020/day11/input"))
+	fmt.Println(doRounds(test, len(input), 5))
+	fmt.Println(doRounds(input, len(input), 5))
 }
